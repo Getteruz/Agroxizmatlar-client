445c3447a7016d331af67e1621de641ed44ae06f
Abdugani Abduqaxxorov <abduganiabdu2@gmail.com>
2023-04-13 12:33:08 +0500

    header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import cls from './Header.module.scss'
+
+export default function Header() {
+    return (
+        <header className={cls.header} >
+            home
+        </header>
+    )
+}
