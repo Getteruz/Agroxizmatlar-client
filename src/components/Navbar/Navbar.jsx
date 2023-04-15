0af6afdea2dd8c1e5eb15d089687d75d8144f4ac
Abdugani Abduqaxxorov <abduganiabdu2@gmail.com>
2023-04-15 17:33:42 +0500

    fixed bugs

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { NavLink, useLocation } from 'react-router-dom'
 import Container from '../ul/container/Container'
 import cls from './Navbar.module.scss'
 import img1 from '../../img/image.svg'
 import img2 from '../../img/image 22.png'
+import getElementSize from '../../utils/getElementSize'
 
 const data = [
     {
@@ -19,23 +20,101 @@ const data = [
         data: '28 Mart 2023'
     }
 ]
+const link = [
+    {
+        id: 1,
+        link: '/',
+        label: 'Bosh sahifa'
+    },
+    {
+        id: 2,
+        link: '/about',
+        label: 'Agentlik haqda'
+    },
+    {
+        id: 3,
+        link: '/service',
+        label: 'Axborot xizmati'
+    },
+    {
+        id: 4,
+        link: '/docs',
+        label: "Me'yoriy hujjatlar"
+    },
+    {
+        id: 5,
+        link: '/data',
+        label: "Ochiq ma'lumotlar"
+    },
+    {
+        id: 6,
+        link: '/Online kredit',
+        label: 'Bosh'
+    },
+    {
+        id: 7,
+        link: '/contact',
+        label: "Bog'lanish"
+    },
+
+]
 
 export default function Navbar() {
+    const [size, setSize] = useState({ width: 0, height: 0, x: 0, y: 0 })
+    const router = useLocation()
+
+
+
+    const func = () => {
+        if (router.pathname === '/') setSize({ width: 0, height: 0, x: 0, y: 0 })
+        const el = document.getElementById(link.find(link => link.link === router.pathname)?.id || link[0]?.id)
+
+        setSize(el ? getElementSize(el) : { x: 0, y: 0 })
+    }
+
+    useEffect(() => {
+        func()
+    }, [router.pathname, link])
     return (
         <div className={cls.Navbar} >
             <Container>
-                <ul className={cls.Navbar__list}>
-                    <li className={cls.Navbar__item}><Link to={'/'}>Bosh sahifa</Link></li>
-                    <li className={cls.Navbar__item}><Link to={'/about'}>Agentlik haqda</Link></li>
-                    <li className={cls.Navbar__item}><Link to={'/service'}>Axborot xizmati</Link></li>
-                    <li className={cls.Navbar__item}><Link to={'/docs'}> Me'yoriy hujjatlar</Link></li>
-                    <li className={cls.Navbar__item}><Link to={'/data'}>Ochiq ma'lumotlar</Link></li>
-                    <li className={cls.Navbar__item}><Link to={'/cridit'}>Online kredit </Link></li>
-                    <li className={cls.Navbar__item}><Link to={'/contact'}>Bog'lanish </Link></li>
-                </ul>
+                <div className={cls.Navbar__list}>
+                    {link?.map(e => (
+                        <NavLink
+                            key={link.id}
+                            id={link.id}
+                            className={cls.Navbar__item}
+                            href={e.link}
+                            active={true}
+                            whiteMode={true}
+                            onMouseOver={(el) => {
+                                setSize(getElementSize(el.target))
+                                el.target.style.color = " #017802"
+
+                            }}
+
+                            onMouseLeave={(el) => {
+                                func()
+                                el.target.style.color = "black"
+                            }}
+                        >
+                            {e.label}
+                        </NavLink>
+
+                    ))}
+                    <div
+                        className={cls.link__active}
+                        style={{
+                            width: size.width,
+                            height: size.height,
+                            top: size.y,
+                            left: size.x
+                        }}
+                    >
+                    </div>
+                </div>
             </Container>
             <Container className={cls.Navbar__bottom}>
-
                 {
                     data?.map(e => (
                         <div className={cls.Navbar__Card} key={e?.id}>
