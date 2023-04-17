1723a5fd461acc6148347fca0de1e8dfd3bf1496
Getter IT Company <130207894+Getteruz@users.noreply.github.com>
2023-04-17 13:34:52 +0500

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -13,7 +13,7 @@ export default function Footer() {
                     <li className={cls.Footer__item}><Link to={'/'}>Публичная оферта</Link></li>
                     <li className={cls.Footer__item}> © 2023 ООО. All rights reserved</li>
                 </ul>
-                <Link to="https://getter.uz/" className={cls.Footer__getter}>Development by: <span>Getter</span></Link>
+                <Link to="https://getter.uz/" className={cls.Footer__getter}>Developed by: <span>Getter</span></Link>
             </Container>
         </footer>
     )
